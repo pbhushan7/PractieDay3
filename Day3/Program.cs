57f32040740bc05a8ea5271af42d8da135e62878
pbhushan7 <bvphulpagar24@gmail.com>
2025-03-26 12:44:54 +0530

    Add project files.

---
+++ Day3/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Constructors in inheritance
+namespace Day3_InheritanceExample1
+{
+    public class Program
+    {
+        static void Main1()
+        {
+            //DerivedClass o1 = new DerivedClass();
+
+            DerivedClass o2 = new DerivedClass(110, 10);
+           
+            Console.WriteLine(o2.j);
+            Console.ReadLine();
+        }
+    }
+
+    public class Baseclass
+    {
+        public int i;
+
+        public Baseclass()
+        {
+            Console.WriteLine("Base class no Param Cons called..");
+            i = 10;
+        }
+        public Baseclass(int i)
+        {
+            Console.WriteLine("Base class Int Cons");
+            this.i = i;
+        }
+
+    }
+
+    public class DerivedClass : Baseclass
+    {
+        public int j;
+
+        public DerivedClass()
+        {
+            Console.WriteLine("Derived class no Param Cons called..");
+            int j = 11;
+            
+        }
+        public DerivedClass(int i, int j) : base(i) 
+        {
+            Console.WriteLine("Derived class int, int cons");
+            this.j = j;
+        }
+    }
+}
+
+// Overloading,Hidding and Overridding
+namespace Day3_InheritanceExample2
+{
+    public class program
+    {
+        public static void Main1()
+        {
+            derived o = new derived();
+            o.Display();// base class method got called
+            o.Display(" ");// Derived class method got called
+
+            o.Display1();//only display derived class method as base class method is hidden
+            o.Display2();//only display derived class method as base class method is overridden
+
+            Subderived o1 = new Subderived();
+            o1.Display1();
+
+            Console.ReadLine();
+
+
+            
+        }
+        public class baseClass
+        {
+            public void Display() //Over loading
+            {
+                Console.WriteLine("Display Baseclass method");
+            }
+            public void Display1() // Hidding
+            {
+                Console.WriteLine("Display Baseclass method 1");
+            }
+            public virtual void Display2() // Overridding
+            {
+                Console.WriteLine("Display Baseclass method 2");
+            }
+        }
+        public class derived : baseClass
+        {
+            public void Display (string s) // here parameter is different
+            {
+                Console.WriteLine("Display DerivedClass method");
+            }
+            public void Display1() //hinding base class method
+            {
+                Console.WriteLine("Display Derived class method 1");
+            }
+            public override void Display2() //overridding base class method in derived class
+            {
+                Console.WriteLine("Display Derived class method 2");
+            }
+        }
+        public class Subderived : derived
+        {
+            public override void Display2() 
+            {
+                Console.WriteLine("Display sudderived method");
+            }
+        }
+
+    }
+
+}
+
+namespace Day3_InheritanceExample3
+{
+    public class program
+    {
+        public static void Main()
+        {
+            //BaseClass O1 = new BaseClass();
+            Derived o = new Derived();
+            o.Display();
+            o.Show();
+            Class2 o2 = new Class2();   
+            o2.Display();
+            o2.Show();
+
+            Console.ReadLine() ;
+
+        }
+        public abstract class BaseClass
+        {
+            public void Display()
+            {
+                Console.WriteLine("Displayyyy");    
+            }
+
+
+        }
+        public class Derived : BaseClass
+        {
+            public void Show()
+            {
+                Console.WriteLine("Showwww");
+            }
+        }
+        public abstract class AbstractClass1
+        {
+            public abstract void Display();
+            public abstract void Show();
+        }
+        public class Class2 : AbstractClass1
+        {
+            public override void Display()
+            {
+                Console.WriteLine("Display");
+            }
+            public override void Show()
+            {
+                Console.WriteLine("Show");
+            }
+        }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces1
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            Class1 o = new Class1();
+            //Method 1 for calling Interface
+            o.Insert();
+            o.Display();    
+
+            //method 2 - Explicit
+            IDbfunctions OIDb;
+            OIDb = o;
+            o.Delete(); 
+
+            //Method 3 -- Implicit 
+            ((IDbfunctions)o).Delete();
+
+            //method 4
+            (o as IDbfunctions).Update();
+
+            Console.ReadLine();
+        }
+        public interface IDbfunctions
+        {
+            void Insert();
+            void Update();
+            void Delete();
+            
+        }
+        public class Class1 : IDbfunctions
+        {
+            public void Insert()
+            {
+                Console.WriteLine("Class1 - IDb.Insert");
+            }
+
+            public void Update()
+            {
+                Console.WriteLine("Class1 - IDb.Update");
+            }
+
+            public void Delete()
+            {
+                Console.WriteLine("Class1 - IDb.Delete");
+            }
+            public void Display()
+            {
+                Console.WriteLine("Display");
+            }
+        }
+    }
+}
+
+namespace Interfaces2
+{
+    internal class Program
+    {
+        static void Main1(string[] args)
+        {
+            Class1 o = new Class1();
+
+            IFilefunctions S;
+            S = o;
+            S.Delete();
+
+            (o as IDbfunctions).Delete();
+
+            IFilefunctions OIfile;
+            OIfile = o;
+            OIfile.Open();
+
+            IDbfunctions IDb;
+            IDb = o;
+            IDb.Delete();
+
+            Console.ReadLine();
+        }
+
+        public interface IDbfunctions
+        {
+            void Insert();
+            void Update();
+            void Delete();
+
+        }
+        public interface IFilefunctions
+        {
+            void Open();
+            void Close();
+            void Delete();
+
+        }
+        public class Class1 : IDbfunctions, IFilefunctions
+        {
+            public void Insert()
+            {
+                Console.WriteLine("Class1 - IDb.Insert");
+            }
+
+            public void Update()
+            {
+                Console.WriteLine("Class1 - IDb.Update");
+            }
+
+            
+            public void Display()
+            {
+                Console.WriteLine("Display");
+            }
+
+            //Wrote down code explicitly to avoid confusion for methods having same name
+
+
+            public void Open()
+            {
+                Console.WriteLine("Class1 - IFile.Open");
+            }
+
+            public void Close()
+            {
+                Console.WriteLine("Class1 - IFile.Close");
+            }
+
+            void IDbfunctions.Delete()
+            {
+                Console.WriteLine("Class1 - IDb.Delete");
+            }
+            void IFilefunctions.Delete()
+            {
+                Console.WriteLine("Class1 - IFile.Delete");
+            }
+        }
+    }
+}
+
+namespace InheritanceInInterface
+{
+    public class Program
+    {
+        static void Main1()
+        {
+            Implement imp = new Implement();
+            imp.Insert();
+            imp.Update();
+            imp.Delete();
+            Console.ReadLine() ;
+
+        }
+
+        public interface IBase
+        {
+            void Insert();
+            void Update();
+        }
+        public interface IDerived : IBase 
+        {
+            void Delete();
+        }
+        public class Implement : IBase, IDerived
+        {
+            public void Delete()
+            {
+                Console.WriteLine("Delete");
+            }
+
+            public void Insert()
+            {
+                Console.WriteLine("Insert");
+            }
+
+            public void Update()
+            {
+                Console.WriteLine("Update");
+            }
+        }
+    }
+}
+
+namespace Bhushan
+{
+    public class Program
+    {
+        public static void Main1()
+        {
+            class1 o = new class1();
+            (o as Interface2).Delete();
+
+
+            o.Insert();
+            o.Update();
+            
+
+            Console.ReadLine();
+        }
+        public interface Interface1
+        {
+            void Insert();
+            void Update();
+            void Delete();
+        }
+        public interface Interface2 : Interface1
+        {
+            void Delete();
+        }
+        public class class1 : Interface1, Interface2
+        {
+            void Interface1.Delete()
+            {
+                Console.WriteLine("Byee1");
+            }
+
+            void Interface2.Delete()
+            {
+                Console.WriteLine("Byee2");
+            }
+
+            public void Insert()
+            {
+                Console.WriteLine("Inserted");
+            }
+
+            public void Update()
+            {
+                Console.WriteLine("Updated");
+            }
+        }
+
+    }
+}
+
